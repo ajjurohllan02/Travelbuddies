a3aec334a8f215dc4c4642dd9388489cc7656ed5
Ajay Kumar <ajaykumar@Ajju.local>
2026-01-08 13:16:13 +0530

    new

--- frontend/src/components/Image-gallery/galleryImages.js
+++ frontend/src/components/Image-gallery/galleryImages.js
@@ -8,6 +8,9 @@ import galleryImg05 from '../../assets/images/IMG-20250922-WA0034.jpg';
 import galleryImg06 from '../../assets/images/IMG-20250922-WA0026.jpg';
 import galleryImg07 from '../../assets/images/IMG_20230120_132355.jpg';
 import galleryImg08 from '../../assets/images/IMG-20250907-WA0026.jpg';
+import galleryImg10 from '../../assets/images/IMG_9784.JPG';
+import galleryImg09 from '../../assets/images/PXL_20251213_014855919.MP.jpg';
+import galleryImg11 from '../../assets/images/20251212_090932.jpg';
 
 const galleryImages =[
     galleryImg01,
@@ -18,6 +21,9 @@ const galleryImages =[
     galleryImg04,
     galleryImg05,
     galleryImg07,
+    galleryImg09,
+    galleryImg10,
+    galleryImg11,
     
 ]
 export default galleryImages;
